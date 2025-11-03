8347d769708389845f53bb4c48546b32fb6e3470
Lucian <lucianjosegoncales@gmail.com>
2025-11-03 16:51:18 -0300

    Implementação de CRUD via API.

--- app/Http/Controllers/Api/SeriesController.php
+++ app/Http/Controllers/Api/SeriesController.php
@@ -5,10 +5,17 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SeriesFormRequest;
 use App\Models\Series;
+use App\Repositories\SeriesRepository;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
+
+    public function __construct(private SeriesRepository $seriesRepository)
+    {
+        
+    }
+
     public function index()
     {
         return Series::all();
@@ -16,6 +23,33 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request){
         //dd($request->all());
-        return response()->json(Series::create($request->all()));
+        //return response()->json(Series::create($request->all()));
+        return response()
+            ->json($this->seriesRepository->add($request), 201);
+    }
+
+    public function show(int $series)
+    {
+        //$series = Series::whereId($series)->with('seasons.episodes')->get();
+        $series = Series::whereId($series)->with('seasons.episodes')
+            ->first();
+        return $series;
+    }
+
+    //public function show(Series $series)
+    //{
+    //    return $series;
+    //}
+
+    public function update(Series $series, SeriesFormRequest $request){
+        $series->fill($request->all());
+        $series->save();
+
+        return $series;
+    }
+
+    public function destroy(int $series){
+        Series::destroy($series);
+        return response()->noContent();
     }
 }
