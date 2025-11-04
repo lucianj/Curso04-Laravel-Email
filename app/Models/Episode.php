51ae423e8c1a7c417aa2c35ef40cfabb4d113a81
Lucian <lucianjosegoncales@gmail.com>
2025-11-04 10:55:02 -0300

    implementação de casts

--- app/Models/Episode.php
+++ app/Models/Episode.php
@@ -11,17 +11,20 @@ class Episode extends Model
     use HasFactory;
     public $timestamps = false;
     protected $fillable = ['number'];
+    protected $casts = [
+        'watched' => 'boolean',
+    ];
 
     public function season()
     {
         return $this->belongsTo(Season::class);
     }
 
-    protected function watched(): Attribute
-    {
-        return new Attribute(
-            get: fn($watched) => (bool) $watched,
-            set: fn($watched) => (bool) $watched,
-        );
-    }
+   // protected function watched(): Attribute
+   // {
+   //     return new Attribute(
+   //         get: fn($watched) => (bool) $watched,
+   //         set: fn($watched) => (bool) $watched,
+   //     );
+   // }
 }
