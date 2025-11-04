94e9989f6cae73a4917551416c069f35faab35f0
Lucian <lucianjosegoncales@gmail.com>
2025-11-04 16:56:21 -0300

    Autenticacao por token

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,9 @@ use App\Http\Controllers\Api\SeriesController;
 use App\Models\Episode;
 use App\Models\Series;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use \Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,23 +23,51 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/series', SeriesController::class);
-Route::get('/series/{series}/seasons', 
-    function (Series $series) {
-        return $series->seasons;
-    }
-);
+Route::middleware('auth:sanctum')->group(function(){
 
-Route::get('/series/{series}/episodes',
-    function (Series $series) {
-        return $series->episodes;
-    }
-);
+    Route::apiResource('/series', SeriesController::class);
+    Route::get('/series/{series}/seasons', 
+        function (Series $series) {
+            return $series->seasons;
+        }
+    );
 
-Route::patch('/episodes/{episode}', function(Episode $episode, Request $request){
-    $episode->watched = $request->watched;
-    $episode->save();
+    Route::get('/series/{series}/episodes',
+        function (Series $series) {
+            return $series->episodes;
+        }
+    );
 
-    return $episode;
-});
+    Route::patch('/episodes/{episode}', function(Episode $episode, Request $request){
+        $episode->watched = $request->watched;
+        $episode->save();
+
+        return $episode;
+    });
 //Route::get('/series', [\App\Http\Controllers\Api\SeriesController::class, 'index']);
+});
+
+Route::post('/login', function(Request $request){
+   $credentials = $request->only(['email', 'password']);
+    //dd($credentials);
+    //$user = \App\Models\User::whereEmail($credentials['email'])
+    //    ->wherePassword(\Illuminate\Support\Facades\Hash::make($credentials['password']))
+    //    ->first();
+   
+   // $user = \App\Models\User::whereEmail($credentials['email'])
+    //    ->first();
+
+   // if ($user === null || !Hash::make($credentials['password'], $user->password) === false){
+   //     return response()->json("Unauthorized", 401);
+   // }
+
+   if(Auth::attempt($credentials) == false){
+        return response()->json("Unauthorized", 401);
+   }
+   
+   $user = Auth::user();
+   $token = $user->createToken('token');
+
+   return response()->json($token->plainTextToken);
+   //dd($user);
+});
